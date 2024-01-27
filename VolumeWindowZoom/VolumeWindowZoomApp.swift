03f23b45a5735d13ac6450cdc5de29a6ee1d0654
Drew Olbrich <drew@lunarskydiving.com>
2024-01-27 12:31:31 -0800

    Add comments

--- VolumeWindowZoom/VolumeWindowZoomApp.swift
+++ VolumeWindowZoom/VolumeWindowZoomApp.swift
@@ -19,18 +19,23 @@ struct VolumeWindowZoomApp: App {
         .windowResizability(.contentSize)
         
         WindowGroup(id: "default-volume") {
+            // A view containing a red sphere that fits within `defaultSize`.
             DefaultVolumeContentView(defaultSize: defaultSize)
         }
         .windowStyle(.volumetric)
         .defaultSize(defaultSize, in: .meters)
 
         WindowGroup(id: "unscaled-volume") {
+            // A view containing a red sphere and the corners of a box of size `defaultSize`.
             UnscaledBoxVolumeContentView(defaultSize: defaultSize)
         }
         .windowStyle(.volumetric)
         .defaultSize(defaultSize, in: .meters)
 
         WindowGroup(id: "scaled-volume") {
+            // A view containing a blue sphere and the corners of a box of size `defaultSize`.
+            // If the user changes their Window Zoom preference in the settings app, the blue
+            // sphere is adaptively scaled to reflect changes in the size of the volume.
             ScaledBoxVolumeContentView(defaultSize: defaultSize)
         }
         .windowStyle(.volumetric)
