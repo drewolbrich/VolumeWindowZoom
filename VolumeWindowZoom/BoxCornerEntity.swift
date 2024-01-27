6672dbdd210d24b1688d0ece68465089cd8fd591
Drew Olbrich <drew@lunarskydiving.com>
2024-01-27 10:29:39 -0800

    Add BoxCornerEntity/update

--- VolumeWindowZoom/BoxCornerEntity.swift
+++ VolumeWindowZoom/BoxCornerEntity.swift
@@ -12,16 +12,17 @@ import UIKit
 /// An entity that draws the corners of a box in a wireframe style.
 class BoxCornerEntity: Entity {
     
-    init(size: Size3D, lineLength: Float = 0.1, lineWidth: Float = 0.01, color: UIColor = .white) {
-        super.init()
-
+    func make(with content: RealityViewContent, for proxy: GeometryProxy3D, defaultSize: Size3D, lineLength: Float = 0.1, lineWidth: Float = 0.01, color: UIColor = .white) {
+        // `make` should not be called twice.
+        assert(children.isEmpty)
+        
         var lineMaterial = PhysicallyBasedMaterial()
         lineMaterial.baseColor = .init(tint: color)
         lineMaterial.roughness = .init(floatLiteral: 1)
         
         func addCorner(at position: SIMD3<Float>) {
             let cornerDirection: SIMD3<Float> = [sign(position.x), sign(position.y), sign(position.z)]
-
+            
             for axis in 0 ..< 3 {
                 var lineSize: SIMD3<Float> = [lineWidth, lineWidth, lineWidth]
                 lineSize[axis] = lineLength
@@ -32,11 +33,11 @@ class BoxCornerEntity: Entity {
                 addChild(lineModelEntity)
             }
         }
-
+        
         for xAxisSign: Float in [-1, 1] {
             for yAxisSign: Float in [-1, 1] {
                 for zAxisSign: Float in [-1, 1] {
-                    var position = SIMD3<Float>(size.vector)/2
+                    var position = SIMD3<Float>(defaultSize.vector)/2
                     position.x *= xAxisSign
                     position.y *= yAxisSign
                     position.z *= zAxisSign
@@ -44,10 +45,14 @@ class BoxCornerEntity: Entity {
                 }
             }
         }
+        
+        update(with: content, for: proxy, defaultSize: defaultSize)
     }
     
-    @MainActor required init() {
-        fatalError("init() has not been implemented")
+    func update(with content: RealityViewContent, for proxy: GeometryProxy3D, defaultSize: Size3D) {
+        let scaledVolumeContentBoundingBox = content.convert(proxy.frame(in: .local), from: .local, to: .scene)
+        let scale = scaledVolumeContentBoundingBox.extents.x/Float(defaultSize.width)
+        self.scale = [1, 1, 1]*scale
     }
-        
+    
 }
--- VolumeWindowZoom/DefaultVolumeContentView.swift
+++ VolumeWindowZoom/DefaultVolumeContentView.swift
@@ -12,20 +12,22 @@ struct DefaultVolumeContentView: View {
     
     let defaultSize: Size3D
     
-    @PhysicalMetric(from: .meters) var oneMeterInPoints = 1
+    @State private var boxCornerEntity = BoxCornerEntity()
     
     var body: some View {
-        ZStack {
+        GeometryReader3D { proxy in
             RealityView { content in
                 let radius: Float = 0.5*Float(min(defaultSize.width, defaultSize.height, defaultSize.depth))
                 
                 let sphereEntity = SphereEntity(radius: radius, color: .systemRed)
                 content.add(sphereEntity)
+
+                boxCornerEntity.make(with: content, for: proxy, defaultSize: defaultSize)
+                content.add(boxCornerEntity)
+            } update: { content in
+                boxCornerEntity.update(with: content, for: proxy, defaultSize: defaultSize)
             }
-            
-            BoxCornerView(defaultSize: defaultSize)
         }
-        .offset(z: -defaultSize.depth/2*oneMeterInPoints)
     }
     
 }
