f87766f8d300274d70d2f94c2fd732bc8b6e0782
Drew Olbrich <drew@lunarskydiving.com>
2024-01-27 06:15:13 -0800

    Initial Commit

---
+++ VolumeWindowZoom/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  VolumeWindowZoom
+//
+//  Created by Drew Olbrich on 1/27/24.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Model3D(named: "Scene", bundle: realityKitContentBundle)
+                .padding(.bottom, 50)
+
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+}
---
+++ VolumeWindowZoom/VolumeWindowZoomApp.swift
@@ -0,0 +1,17 @@
+//
+//  VolumeWindowZoomApp.swift
+//  VolumeWindowZoom
+//
+//  Created by Drew Olbrich on 1/27/24.
+//
+
+import SwiftUI
+
+@main
+struct VolumeWindowZoomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
