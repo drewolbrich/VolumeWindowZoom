b98e63c0ae8b25e8d6289b8f6fbf23e04876730c
Drew Olbrich <drew@lunarskydiving.com>
2024-01-27 12:56:11 -0800

    Update comments

--- VolumeWindowZoom/VolumeWindowZoomApp.swift
+++ VolumeWindowZoom/VolumeWindowZoomApp.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 /// A visionOS app that demonstrates how to use `GeometryReader3D` to scale the
 /// contents of a volumetric window group to reflect the user's Window Zoom
-/// preference selected in the Settings app.
+/// preference as selected in the Settings app.
 ///
 /// In the visionOS Settings app, the Display > Appearance > Window Zoom preference
 /// (with options Small, Medium, Large, and Extra Large) scales the size of the
