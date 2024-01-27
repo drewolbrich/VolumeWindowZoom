4d484bf1cbdc00b8bebd90ce454cbbb0f636e380
Drew Olbrich <drew@lunarskydiving.com>
2024-01-27 11:24:45 -0800

    Add comments

--- VolumeWindowZoom/ResponsiveBoxCornersEntity.swift
+++ VolumeWindowZoom/ResponsiveBoxCornersEntity.swift
@@ -22,6 +22,7 @@ class ResponsiveBoxCornersEntity: Entity {
     
     private var color: UIColor = .magenta
     
+    /// This method must be called from the RealityView's `make` closure.
     func make(with content: RealityViewContent, for proxy: GeometryProxy3D, defaultSize: Size3D, lineLength: Float = 0.1, lineWidth: Float = 0.01, color: UIColor = .white) {
         // `make` should not be called twice.
         assert(children.isEmpty)
@@ -34,6 +35,7 @@ class ResponsiveBoxCornersEntity: Entity {
         addCorners(with: content, for: proxy)
     }
     
+    /// This method must be called from the RealityView's `update` closure.
     func update(with content: RealityViewContent, for proxy: GeometryProxy3D) {
         // `make(with:for:defaultSize:lineLength:lineWidth:color:)` must have been called first.
         assert(!children.isEmpty)
