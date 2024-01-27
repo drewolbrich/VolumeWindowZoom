e467162d9e812877854bbb21629062ac4a766ef9
Drew Olbrich <drew@lunarskydiving.com>
2024-01-27 10:14:20 -0800

    Scale BoxCornerView with Window Zoom

--- VolumeWindowZoom/BoxCornerView.swift
+++ VolumeWindowZoom/BoxCornerView.swift
@@ -14,15 +14,27 @@ struct BoxCornerView: View {
     
     let defaultSize: Size3D
 
+    @State private var boxCornerEntity: Entity?
+
     var body: some View {
-        GeometryReader { proxy in
-            RealityView { content in                
+        GeometryReader3D { proxy in
+            RealityView { content in
                 let boxCornerEntity = BoxCornerEntity(size: defaultSize)
+                self.boxCornerEntity = boxCornerEntity
+                scaleEntity(boxCornerEntity, with: content, for: proxy, defaultSize: defaultSize)
                 content.add(boxCornerEntity)
             } update: { content in
-                
+                if let boxCornerEntity {
+                    scaleEntity(boxCornerEntity, with: content, for: proxy, defaultSize: defaultSize)
+                }
             }
         }
     }
     
+    private func scaleEntity(_ entity: Entity, with content: RealityViewContent, for proxy: GeometryProxy3D, defaultSize: Size3D) {
+        let scaledVolumeContentBoundingBox = content.convert(proxy.frame(in: .local), from: .local, to: .scene)
+        let scale = scaledVolumeContentBoundingBox.extents.x/Float(defaultSize.width)
+        entity.scale = [1, 1, 1]*scale
+    }
+
 }
