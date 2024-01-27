c3c952eed60210a93d4f94eee97fdb3757c1d89c
Drew Olbrich <drew@lunarskydiving.com>
2024-01-27 11:04:31 -0800

    Add comments

--- VolumeWindowZoom/DefaultVolumeContentView.swift
+++ VolumeWindowZoom/DefaultVolumeContentView.swift
@@ -8,6 +8,16 @@
 import SwiftUI
 import RealityKit
 
+/// A view that displays a red sphere in a `RealityView`.
+///
+/// When the sphere is created, it is sized to fit exactly within `defaultSize`,
+/// which should correspond to the value passed to the volumetric window group's
+/// `defaultSize(_:in:)` view modifier.
+///
+/// Because `GeometryReader3D` is not used, if this view is placed in a volume and
+/// the user changes their Window Zoom preference in the Settings app, the sphere
+/// may be clipped if the volume is too small or surrounded by extra padding if the
+/// volume is too large.
 struct DefaultVolumeContentView: View {
     
     let defaultSize: Size3D
